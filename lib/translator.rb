a921e3ce940246925816c62e1fb699e15272597e
Garrett Burns <garrettburns@mac.com>
2019-09-15 02:23:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,6 @@ require "yaml"
 def load_library(path)
   emoticons = YAML.load_file(path)
   emoticon_list = {'get_meaning' => {}, 'get_emoticon' => {}}
-  emoticon_list['get_meaning'] = emoticons
   
 end
 
